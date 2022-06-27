1f29abcb2b09d9f127ee95e17e5e716901653303
Fabio Pereira <fabiojr933@gmail.com>
2022-06-27 17:16:21 -0400

    ..

--- mobile/screens/ofx/lista.jsx
+++ mobile/screens/ofx/lista.jsx
@@ -17,7 +17,7 @@ import {
   Flex,
   Spinner,
   Box,
-  Input, 
+  Input,
 } from "native-base";
 import { AntDesign } from "@expo/vector-icons";
 import FloatingLabelInput from "./components/FloatingLabelInput";
@@ -28,33 +28,34 @@ import { useNavigation } from "@react-navigation/native";
 import SyncStorage from "@react-native-async-storage/async-storage";
 import { ScrollView, TextInput } from 'react-native';
 
+
 export function OFXForm({ props }) {
- 
+
   const [listaDados, setListaDados] = useState([]);
   const [nomeBanco, setNomeBanco] = useState('');
   const [numeroConta, setNumeroConta] = useState('');
   const [numeroBanco, setNumeroBanco] = useState('');
-  const [tempo, setTempo] = useState(false);
+  const [tempo, setTempo] = useState(true);
   const [load, setLoad] = useState(true);
   const [listaDespesa, setListaDespesa] = useState([]);
   const [listaReceita, setListaReceita] = useState([]);
   const navigation = useNavigation();
 
 
-  
-  useEffect(() => {    
+
+  useEffect(() => {
     async function loadData() {
-      setListaDados(props.route.params.dados.BANKTRANLIST.STMTTRN)    
+      setListaDados(props.route.params.dados.BANKTRANLIST.STMTTRN)
       setNomeBanco(props.route.params.dados.nomeBanco);
       setNumeroBanco(props.route.params.dados.numeroBanco);
       setNumeroConta(props.route.params.dados.numeroConta);
     }
-    loadData();   
-  },[]);
+    loadData();
+  }, []);
 
 
-  useEffect(() => {    
-    async function listar(){
+  useEffect(() => {
+    async function listar() {
       var configDespesa = {};
       var configReceita = {};
       await SyncStorage.getItem('@user').then((value) => {
@@ -76,196 +77,197 @@ export function OFXForm({ props }) {
           },
         };
 
+        console.log(configDespesa)
+        axios(configDespesa).then((resposta) => {
+          console.log(resposta)
+          setListaDespesa(resposta.data);
+        });
+        axios(configReceita).then((resposta) => {
+          console.log(resposta)
+          setListaReceita(resposta.data);
+        });
       });
-      console.log(configDespesa)   
-      axios(configDespesa).then((resposta) => {
-        console.log(resposta)
-        setListaDespesa(resposta.data);
-      });
-      axios(configReceita).then((resposta) => {
-        console.log(resposta)
-        setListaReceita(resposta.data);
-      });
+      setTempo(false);
     }
+    //    setTempo(false);
     listar();
-  },[]);
-
- 
+  }, []);
 
 
-  function ReceitaDespesa(v) { 
+  
+
+  function ReceitaDespesa(v) {
     if (v == 'DEBIT') {
       return (
         <Select placeholder="Selecione uma Despesa">
-        { listaDespesa.map((d) =>(         
-        <Select.Item label={d.despesa} value={d.id} key={d.id}/>        
-       ) )}                    
-  </Select>
+          {listaDespesa.map((d) => (
+            <Select.Item label={d.despesa} value={d.id} key={d.id} />
+          ))}
+        </Select>
       )
-    }else{
+    } else {
       return (
         <Select placeholder="Selecione uma Receita">
-        { listaReceita.map((d) =>(                   
-         <Select.Item label={d.receita} value={d.id} key={d.id}/>        
-         ) )}                    
-      </Select>
+          {listaReceita.map((d) => (
+            <Select.Item label={d.receita} value={d.id} key={d.id} />
+          ))}
+        </Select>
       )
-      
-    }   
+    }
   }
 
 
 
 
 
-if(tempo == true){
-  return ( 
-  <HStack space={2} justifyContent="center" marginTop='200'>
+  if (tempo == true) {
+    return (
+      <HStack space={2} justifyContent="center" marginTop='200'>
         <Spinner size="sm" color="#ffffff" />
         <Heading color="#ffffff" fontSize="21">
           Processando os dados...
-        </Heading>        
+        </Heading>
       </HStack>
-      )
-}else{
-  return (
-    <KeyboardAwareScrollView
-      contentContainerStyle={{ flexGrow: 1 }}
-      style={{ flex: 1 }}
-    >
-      <VStack
-        flex="1"
-        px="6"
-        py="9"
-        _light={{
-          bg: "white",
-        }}
-        _dark={{
-          bg: "coolGray.800",
-        }}
-        space="3"
-        justifyContent="space-between"
-        borderTopRightRadius={{
-          base: "2xl",
-          md: "xl",
-        }}
-        borderBottomRightRadius={{
-          base: "0",
-          md: "xl",
-        }}
-        borderTopLeftRadius={{
-          base: "2xl",
-          md: "0",
-        }}
+    )
+  } else {
+    return (
+      <KeyboardAwareScrollView
+        contentContainerStyle={{ flexGrow: 1 }}
+        style={{ flex: 1 }}
       >
-        <VStack space="7">
-          <Hidden till="md">
-            <Text fontSize="lg" fontWeight="normal">
-            
-            </Text>
-          </Hidden>
-          <VStack>
-            <VStack space="3">
-              <VStack
-                space={{
-                  base: "7",
-                  md: "4",
-                }}
-              >  
-                <Button
-                 // onPressOut={openDocumentFile}
-               
-                  mt="5"
-                  size="md"
-                  borderRadius="4"
-                  _text={{
-                    fontWeight: "medium",
-                  }}
-                  _light={{
-                    bg: "primary.900",
-                  }}
-                  _dark={{
-                    bg: "primary.700",
+        <VStack
+          flex="1"
+          px="6"
+          py="9"
+          _light={{
+            bg: "white",
+          }}
+          _dark={{
+            bg: "coolGray.800",
+          }}
+          space="3"
+          justifyContent="space-between"
+          borderTopRightRadius={{
+            base: "2xl",
+            md: "xl",
+          }}
+          borderBottomRightRadius={{
+            base: "0",
+            md: "xl",
+          }}
+          borderTopLeftRadius={{
+            base: "2xl",
+            md: "0",
+          }}
+        >
+          <VStack space="7">
+            <Hidden till="md">
+              <Text fontSize="lg" fontWeight="normal">
+
+              </Text>
+            </Hidden>
+            <VStack>
+              <VStack space="3">
+                <VStack
+                  space={{
+                    base: "7",
+                    md: "4",
                   }}
                 >
-                 Fazer os Lançamento
-                </Button>
-              </VStack>                  
+                  <Button
+                    // onPressOut={openDocumentFile}
+
+                    mt="5"
+                    size="md"
+                    borderRadius="4"
+                    _text={{
+                      fontWeight: "medium",
+                    }}
+                    _light={{
+                      bg: "primary.900",
+                    }}
+                    _dark={{
+                      bg: "primary.700",
+                    }}
+                  >
+                    Fazer os Lançamento
+                  </Button>
+                </VStack>
+              </VStack>
             </VStack>
+
+  
+              {listaDados.map((v, k) => (
+
+                <>
+                  <Divider bg="primary.700" thickness="2" mx="2" />
+                  <HStack >
+
+                    <Divider bg="primary.700" thickness="2" mx="2" orientation="vertical" />
+                    <TextInput fontSize={10} variant="unstyled" placeholder="Unstyled" color="coolGray.800" textAlign='center' justifyContent='center' editable={false} selectTextOnFocus={false} >
+                      {v.DTPOSTED}
+                    </TextInput >
+                    <Divider bg="primary.700" thickness="2" mx="2" orientation="vertical" />
+                    <TextInput fontSize={10} color="coolGray.800" textAlign='center' justifyContent='center' editable={false} selectTextOnFocus={false}>
+                      Id: {v.FITID}
+                    </TextInput>
+                    <Divider bg="primary.700" thickness="2" mx="2" orientation="vertical" />
+                    <TextInput fontSize={10} color="coolGray.800" textAlign='center' justifyContent='center' editable={false} selectTextOnFocus={false}>
+                      Protocolo {v.CHECKNUM}
+                    </TextInput>
+                  </HStack>
+                  <HStack space={2} justifyContent="center">
+
+                    {(() => {
+                      if (v.TRNTYPE == 'DEBIT') {
+                        return (
+                          <TextInput color='#be1c16' fontSize={16} mt="1" fontWeight="medium" textAlign='center' editable={false} selectTextOnFocus={false}>
+                            {v.TRNTYPE}
+                          </TextInput>
+                        )
+                      } else {
+                        return (
+                          <TextInput color='#050f9c' fontSize={16} mt="1" fontWeight="medium" textAlign='center' editable={false} selectTextOnFocus={false}>
+                            {v.TRNTYPE}
+                          </TextInput>
+                        )
+                      }
+                    })()}
+
+                    {(() => {
+                      if (v.TRNTYPE == 'DEBIT') {
+                        return (
+                          <TextInput color='#be1c16' mt="1" fontWeight="medium" fontSize={16} textAlign='center' editable={false} selectTextOnFocus={false}>
+                            R$ {v.TRNAMT}
+                          </TextInput>
+                        )
+                      } else {
+                        return (
+                          <TextInput color='#050f9c' mt="1" fontWeight="medium" fontSize={16} textAlign='center' editable={false} selectTextOnFocus={false}>
+                            R$ {v.TRNAMT}
+                          </TextInput>
+                        )
+                      }
+                    })()}
+
+                  </HStack>
+                  <TextInput mt="2" fontSize={12} color="gray.800" textAlign='center' editable={false} selectTextOnFocus={false}>
+                    {nomeBanco}
+                  </TextInput>
+                  <Flex>
+                    <TextInput mt="2" fontSize={12} fontWeight="medium" color="gray.800" editable={false} selectTextOnFocus={false}>
+                      {v.MEMO}
+                    </TextInput>
+                  </Flex>
+                  {ReceitaDespesa(v.TRNTYPE)}
+                </>
+              ))}
+             
           </VStack>
-
-                  
-
-          { listaDados.map((v, k) => ( 
- 
- <>
- <Divider bg="primary.700" thickness="2" mx="2" />
-     <HStack > 
-     
-     <Divider bg="primary.700" thickness="2" mx="2" orientation="vertical" />
-       <TextInput fontSize={10} variant="unstyled" placeholder="Unstyled"  color="coolGray.800" textAlign='center' justifyContent='center' editable={false} selectTextOnFocus={false} >
-         {v.DTPOSTED}  
-       </TextInput >
-       <Divider bg="primary.700" thickness="2" mx="2" orientation="vertical" />
-       <TextInput fontSize={10} color="coolGray.800" textAlign='center' justifyContent='center' editable={false} selectTextOnFocus={false}>
-         Id: {v.FITID}  
-       </TextInput>
-       <Divider bg="primary.700" thickness="2" mx="2" orientation="vertical" />
-       <TextInput fontSize={10} color="coolGray.800" textAlign='center' justifyContent='center' editable={false} selectTextOnFocus={false}>
-         Protocolo {v.CHECKNUM}  
-       </TextInput>
-     </HStack>
-     <HStack space={2} justifyContent="center">
- 
-     {(() => {
-          if(v.TRNTYPE == 'DEBIT') {
-           return (
-           <TextInput color='#be1c16' fontSize={16}  mt="1" fontWeight="medium" textAlign='center' editable={false} selectTextOnFocus={false}>
-           {v.TRNTYPE}
-         </TextInput>
-           )
-         }else{
-           return (
-           <TextInput color='#050f9c' fontSize={16}  mt="1" fontWeight="medium"  textAlign='center' editable={false} selectTextOnFocus={false}>
-       {v.TRNTYPE}
-     </TextInput>
-           )
-         }
-       })()}
-      
-     {(() => {
-       if(v.TRNTYPE == 'DEBIT'){
-         return(
-           <TextInput color='#be1c16' mt="1" fontWeight="medium" fontSize={16} textAlign='center' editable={false} selectTextOnFocus={false}>
-           R$ {v.TRNAMT} 
-           </TextInput>
-         )
-       }else{
-         return(
-           <TextInput color='#050f9c' mt="1" fontWeight="medium" fontSize={16} textAlign='center' editable={false} selectTextOnFocus={false}>
-           R$ {v.TRNAMT} 
-           </TextInput>
-         )
-       }
-     })()}
-     
-     </HStack>
-     <TextInput mt="2" fontSize={12} color="gray.800" textAlign='center' editable={false} selectTextOnFocus={false}>
-       {nomeBanco}
-     </TextInput>
-     <Flex>
-       <TextInput mt="2" fontSize={12} fontWeight="medium" color="gray.800" editable={false} selectTextOnFocus={false}>
-       {v.MEMO}
-       </TextInput>     
-     </Flex> 
-     {ReceitaDespesa(v.TRNTYPE)}   
-   </>
- ))}
-
         </VStack>
-      </VStack>
-    </KeyboardAwareScrollView>
-  )};
+      </KeyboardAwareScrollView>
+    )
+  };
 }
 export default function ofx(props) {
   return (
@@ -337,7 +339,7 @@ export default function ofx(props) {
                   color="coolGray.50"
                   textAlign="center"
                 >
-                  Falta pouco para fazer o lancamento
+                  Falta pouco para fazer o lançamento
                 </Text>
                 <Text
                   textAlign="center"
@@ -350,7 +352,7 @@ export default function ofx(props) {
                     color: "primary.300",
                   }}
                 >
-                   Agora você precisa vincular os lançamentos com seu devidos fluxo financeiro(tipos de despesa e receita);
+                  Agora você precisa vincular os lançamentos com seu devidos fluxo financeiro(tipos de despesa e receita);
                 </Text>
               </VStack>
             </VStack>
